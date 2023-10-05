56ea1116e2dbef667acbf29085c2a10b8fa1d64f
Shafi <jonyszone@gmail.com>
2023-10-05 21:47:19 +0600

    resume link changed

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Android Application Developer 🚀 having an experience of building Mobile applications with Java / Kotlin / Android / Flutter and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1geGWKsjcYStmcPaNco94e-nIoGRtO0EA/view?usp=drive_link", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1LG2h1VHCMvjhe_8PHtbJMZBTarFYhXVa/view?usp=drive_link", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
